4b895912efd7faabeb89dd98d98950d5fad8118a
Federico Aletti <fedeale2000@gmail.com>
2024-01-21 03:32:44 -0300

    adding firebase

--- service/products.ts
+++ service/products.ts
@@ -1,18 +1,38 @@
+import { Product } from '@/types';
 import { collection, addDoc } from "firebase/firestore"
 import { db } from "./firebase"
 
 export const addProduct = async () => {
 	try {
-		const docRef = await addDoc(collection(db, "brands"), {
-			name: "Adalya",
-			price: {
-				50: 2300,
-                200: 8000
+		const docRef = await addDoc(collection(db, "products"), {
+			name: "Blue Melon",
+			description: "Arandanos, melon y menta",
+			thumbnail: "gs://narguicba-2d5f8.appspot.com/productos/tabacos/adalya-bluemelon.png",
+			brand: "Adalya",
+			price: 4750,
+			category: {
+				reference: "categories",
+				id: "VZhAMjGlEuEA9tPWT9Mu"
 			},
-			logo: "",
+			stock: 10,
+			isAvailable: true,
+			discount: 0
 		})
 		console.log("Document written with ID: ", docRef.id)
 	} catch (e) {
 		console.error("Error adding document: ", e)
 	}
+}
+
+type Product = {
+	id: string
+	name: string
+	description: string
+	thumbnail: string
+	brand: Brand
+	price: number
+	category: Category
+	stock: number
+	isAvailable: boolean
+	discount?: number
 }
--- types/index.ts
+++ types/index.ts
@@ -14,6 +14,7 @@ export type Product = {
 	category: Category
 	stock: number
 	isAvailable: boolean
+	discount?: number
 }
 
 export type ProductInCard = Product & {
